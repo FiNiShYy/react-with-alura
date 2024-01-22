e8aa9b7ca23952be16f31fa9123c491cf5b4a28b
Kauã Gabriel <kauagabrielramkg@gmail.com>
2024-01-22 10:56:59 -0300

    Update

--- src/components/DropdownList/index.js
+++ src/components/DropdownList/index.js
@@ -5,7 +5,8 @@ const DropdownList = (props) => {
     return (
         <div className="dropdownlist">
             <label>{props.label}</label>
-            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.obrigatorio} value={props.value}>
+            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.obrigatorio} value={props.valor}>
+                <option value="" hidden>Selecione um time...</option>
                 {props.items.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
